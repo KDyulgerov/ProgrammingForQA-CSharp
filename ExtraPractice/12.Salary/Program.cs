ce48ee13b6f8890e88e81c2db538be45760a3f6d
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-05 17:46:42 +0300

    Updated
    Updated

---
+++ ExtraPractice/12.Salary/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _12.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var openTabs = int.Parse(Console.ReadLine());
+            var salary = int.Parse(Console.ReadLine());
+
+            string currentSite = "";
+
+            int i = 1;
+            while (salary > 0 && i <= openTabs) 
+            {
+                currentSite = Console.ReadLine();
+                i++;
+
+                if (currentSite == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (currentSite == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (currentSite == "Reddit")
+                {
+                    salary -= 50;
+                }
+            }
+
+            if (salary > 0)
+            {
+                Console.WriteLine($"{salary:F0}");
+            }
+            else
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            
+        }
+    }
+}
---
+++ ExtraPractice/13.Oscars/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _13.Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameActor = Console.ReadLine();
+            var startingPoints = double.Parse(Console.ReadLine());
+            var juryNum = int.Parse(Console.ReadLine());
+
+            string nameJury = "";
+            var juryPoints = 0.00;
+
+            int i = 1;
+            while (i <= juryNum && startingPoints <= 1250.5)
+            {
+                nameJury = Console.ReadLine();
+                juryPoints = double.Parse(Console.ReadLine());
+
+                startingPoints += (juryPoints * nameJury.Length / 2);
+
+                i++;
+            }
+
+            var diff = (1250.5 - startingPoints);
+
+            if (startingPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {nameActor} got a nominee for leading role with {startingPoints:F1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {nameActor} you need {diff:F1} more!");
+            }
+        }
+    }
+}
