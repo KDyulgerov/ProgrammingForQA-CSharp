19526d6acce95a8cf049b59394166c5404b940a6
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-09 11:28:03 +0300

    Updated
    Updated

---
+++ ExtraPractice/15.SpecialNum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _15.SpecialNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine()); // example: 521
+            var startingNum = number;
+
+            bool isSpecial = true;
+
+            while (number > 0)
+            {
+                var lastDigit = number % 10; // = 1
+                if (startingNum % lastDigit != 0)
+                {
+                    isSpecial = false;
+                    break;
+                }
+
+                number /= 10;
+            }
+
+            if (isSpecial)
+            {
+                Console.WriteLine($"{startingNum} is special");
+            }
+            else
+            {
+                Console.WriteLine($"{startingNum} is not special");
+            }
+        }
+    }
+}
---
+++ ExtraPractice/15.TennisRanklist/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _16.TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var compNumber = int.Parse(Console.ReadLine());
+            var startingPoints = int.Parse(Console.ReadLine());
+            var tournamentPoints = 0;
+            var wins = 0;
+
+            for (int i = 1; i <= compNumber; i++)
+            {
+                string status = Console.ReadLine();
+
+                switch (status)
+                {
+                    case "W":
+                    {
+                        tournamentPoints += 2000;
+                        wins++;
+                        break;
+                    }
+                    case "F":
+                    {
+                        tournamentPoints += 1200;
+                        break;
+                    }
+                    case "SF":
+                    {
+                        tournamentPoints += 720;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Final points: {startingPoints + tournamentPoints}");
+            Console.WriteLine($"Average points: {tournamentPoints / compNumber}");
+            Console.WriteLine($"{wins * 1.0 / compNumber * 100:F2}%");
+        }
+    }
+}
