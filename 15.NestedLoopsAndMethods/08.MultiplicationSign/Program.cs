8fa51b2d7c645a9cf838aafd5472769304e4f6c5
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-14 18:15:18 +0300

    ExercisesPractice
    ExercisesPractice

---
+++ 15.NestedLoopsAndMethods/08.MultiplicationSign/Program.cs
@@ -0,0 +1,31 @@
+﻿static void MultiplicationSign(int num1, int num2, int num3)
+{
+    string sign = "";
+
+    if (num1 > 0 && num2 > 0 && num3 > 0
+        || num1 > 0 && num2 < 0 && num3 < 0
+        || num1 < 0 && num2 < 0 && num3 > 0
+        || num1 < 0 && num2 > 0 && num3 < 0)
+    {
+        sign = "positive";
+    }
+    else if (num1 < 0 && num2 < 0 && num3 < 0
+             || num1 > 0 && num2 > 0 && num3 < 0
+             || num1 > 0 && num2 < 0 && num3 > 0
+             || num1 < 0 && num2 > 0 && num3 > 0)
+    {
+        sign = "negative";
+    }
+    else if (num1 == 0 || num2 == 0 || num3 == 0)
+    {
+        sign = "zero";
+    }
+
+    Console.WriteLine(sign);
+}
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+MultiplicationSign(num1, num2, num3);
---
+++ 15.NestedLoopsAndMethods/09.PasswordValidator/Program.cs
@@ -0,0 +1,60 @@
+﻿static bool LettersAndDigitsChecker(string password)
+{
+    for (int i = 0; i <= password.Length - 1; i++)
+    {
+        char currentLetter = password[i];
+        if (char.IsLetterOrDigit(currentLetter) == false)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool DigitsInString(string password)
+{
+    int count = 0;
+
+    for (int i = 0; i <= password.Length - 1; i += 1)
+    {
+        char currentValue = password[i];
+
+        if (char.IsDigit(currentValue))
+        {
+            count++;
+        }
+    }
+
+    if (count < 2)
+    {
+        return false;
+    }
+    return true;
+}
+
+string password = Console.ReadLine();
+
+bool isValid = true;
+
+if (password.Length < 6 || password.Length > 10)
+{
+    Console.WriteLine($"Password must be between 6 and 10 characters");
+    isValid = false;
+}
+
+if (LettersAndDigitsChecker(password) == false)
+{
+    Console.WriteLine("Password must consist only of letters and digits");
+    isValid = false;
+}
+if (DigitsInString(password) == false)
+{
+    Console.WriteLine("Password must have at least 2 digits");
+    isValid = false;
+}
+
+
+if (isValid)
+{
+    Console.WriteLine("Password is valid");
+}
