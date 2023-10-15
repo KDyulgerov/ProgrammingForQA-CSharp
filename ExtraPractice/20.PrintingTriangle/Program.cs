9ddef0136a7dcf1390e2a9b28b43d531cb674133
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-15 22:05:56 +0300

    Updated
    Updated

---
+++ ExtraPractice/20.PrintingTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i += 1)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine();
+}
+
+static void PrintTriangle(int number)
+{
+    for (int currentLine = 1; currentLine <= number; currentLine += 1)
+    {
+        PrintLine(1, currentLine);
+    }
+
+    for (int currentLine = number - 1; currentLine >= 1; currentLine -= 1)
+    {
+        PrintLine(1, currentLine);
+    }
+}
+int number = int.Parse(Console.ReadLine());
+
+PrintTriangle(number);
+
---
+++ ExtraPractice/21.Clock/Program.cs
@@ -0,0 +1,31 @@
+﻿
+for (int hour = 00; hour <= 23; hour++)
+{
+    for (int minute = 1; minute <= 59; minute++)
+    {
+        for (int second = 1; second <= 59; second++)
+        {
+            string typeMin = "";
+            string typeSec = "";
+            string typeHour = "";
+
+            if (minute <= 9)
+            {
+                typeMin = "0";
+            }
+
+            if (second <= 9)
+            {
+                typeSec = "0";
+            }
+
+            if (hour == 0 || hour < 10)
+            {
+                typeHour = "0";
+            }
+            Console.Clear();
+            Console.Write($"{typeHour}{hour}:{typeMin}{minute}:{typeSec}{second}");
+            Thread.Sleep(1000);
+        }
+    }
+}
