a85a9a832ce8006a789b3310dd1fce578652d07a
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-18 18:01:18 +0300

    LabPractice
    LabPractice

---
+++ 16.Arrays/06.CommonElements/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] firstNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+for (int i = 0; i <= firstNumbers.Length - 1; i += 1)
+{
+    for (int j = 0; j <= secondNumbers.Length - 1; j += 1)
+    {
+        if (firstNumbers[i] == secondNumbers[j])
+        {
+            Console.Write(firstNumbers[i] + " ");
+        }
+    }
+}
