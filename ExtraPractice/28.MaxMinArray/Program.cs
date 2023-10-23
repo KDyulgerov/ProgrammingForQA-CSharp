280ac9ad34ee8ad95861613730529d966d6ca909
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-23 11:42:08 +0300

    Updated
    Updated

---
+++ ExtraPractice/28.MaxMinArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int minNum = int.MaxValue;
+int maxNum = int.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < minNum)
+    {
+        minNum = numbers[i];
+    }
+
+    if (numbers[i] > maxNum)
+    {
+        maxNum = numbers[i];
+    }
+}
+
+Console.WriteLine($"Max number is: {maxNum}");
+Console.WriteLine($"Min number is: {minNum}");
