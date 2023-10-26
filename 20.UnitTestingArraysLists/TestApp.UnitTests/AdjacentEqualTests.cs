8f5545c4fd176a5061e0f000c8e190eeace7504a
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-26 09:55:15 +0300

    ExercisePractice
    ExercisePractice

---
+++ 20.UnitTestingArraysLists/TestApp.UnitTests/AdjacentEqualTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class AdjacentEqualTests
+{
+    [Test]
+    public void Test_Sum_InputIsNull_ShouldThrowArgumentException()
+    {
+        List<int>? nullList = null;
+
+        Assert.That(() => AdjacentEqual.Sum(nullList), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_Sum_InputIsEmptyList_ShouldReturnEmptyString()
+    {
+        List<int> emptyList = new();
+
+        string result = AdjacentEqual.Sum(emptyList);
+
+        List<int> expected = new();
+        CollectionAssert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_Sum_NoAdjacentEqualNumbers_ShouldReturnOriginalList()
+    {
+        List<int> noAdjacentNums = new() { 1, 2, 3, 4, 5 };
+
+        string result = AdjacentEqual.Sum(noAdjacentNums);
+
+        Assert.That(result, Is.EqualTo("1 2 3 4 5"));
+    }
+
+    [Test]
+    public void Test_Sum_AdjacentEqualNumbersExist_ShouldReturnSummedList()
+    {
+        List<int> adjacentNums = new() { 1, 1, 2, 3, 4, 5 };
+
+        string result = AdjacentEqual.Sum(adjacentNums);
+
+        Assert.That(result, Is.EqualTo("4 3 4 5"));
+    }
+
+    [Test]
+    public void Test_Sum_AllNumbersAreAdjacentEqual_ShouldReturnSingleSummedNumber()
+    {
+        List<int> allAdjacent = new() { 1, 1, 2, 4, 8 };
+
+        string result = AdjacentEqual.Sum(allAdjacent);
+
+        Assert.That(result, Is.EqualTo("16"));
+    }
+
+    [Test]
+    public void Test_Sum_AdjacentEqualNumbersAtBeginning_ShouldReturnSummedList()
+    {
+        List<int> beginningEqual = new() { 1, 1, 2, 4, 5, 3, 2, 3 };
+
+        string result = AdjacentEqual.Sum(beginningEqual);
+
+        Assert.That(result, Is.EqualTo("8 5 3 2 3"));
+    }
+
+    [Test]
+    public void Test_Sum_AdjacentEqualNumbersAtEnd_ShouldReturnSummedList()
+    {
+        List<int> endEqual = new() { 1, 3, 4, 1, 1, 2 };
+
+        string result = AdjacentEqual.Sum(endEqual);
+
+        Assert.That(result, Is.EqualTo("1 3 8"));
+    }
+
+    [Test]
+    public void Test_Sum_AdjacentEqualNumbersInMiddle_ShouldReturnSummedList()
+    {
+        List<int> middleEqual = new() { 1, 3, 4, 4, 5 };
+
+        string result = AdjacentEqual.Sum(middleEqual);
+
+        Assert.That(result, Is.EqualTo("1 3 8 5"));
+    }
+}
---
+++ 20.UnitTestingArraysLists/TestApp.UnitTests/EvenOddSubtractionTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class EvenOddSubtractionTests
+{
+    [Test]
+    public void Test_FindDifference_InputIsEmpty_ShouldReturnZero()
+    {
+        int[] emptyArray = Array.Empty<int>();
+
+        int result = EvenOddSubtraction.FindDifference(emptyArray);
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasOnlyEvenNumbers_ShouldReturnEvenSum()
+    {
+        int[] evenElements = { 2, 4, 6, 8 };
+
+        int result = EvenOddSubtraction.FindDifference(evenElements);
+
+        Assert.That(result, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasOnlyOddNumbers_ShouldReturnOddSum()
+    {
+        int[] oddElements = { 1, 3, 5, 7, 9 };
+
+        int result = EvenOddSubtraction.FindDifference(oddElements);
+
+        Assert.That(result, Is.EqualTo(25));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasMixedNumbers_ShouldReturnDifference()
+    {
+        int[] mixedElements = { 1, 2, 3, 4, 5, };
+
+        int result = EvenOddSubtraction.FindDifference(mixedElements);
+
+        Assert.That(result, Is.EqualTo(3));
+    }
+}
