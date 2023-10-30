79a5902bf0dda589a4a6e27158eb451e245552c3
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-30 23:53:21 +0200

    Updated
    Updated

---
+++ ExtraPractice/31.SumFactorialEvenDigitsMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(CalculateFactorialEvenNum(number));
+
+static int CalculateFactorialEvenNum(int number)
+{
+    string numberStr = number.ToString();
+
+    int sum = 0;
+
+    foreach(char c in numberStr)
+    {
+        int currentDigit = int.Parse(c.ToString());
+
+        int factorial = currentDigit;
+
+        if (currentDigit % 2 == 0)
+        {
+            for (int i = 1; i < currentDigit; i++)
+            {
+                factorial *= i;
+            }
+            sum += factorial;
+        }
+    }
+
+    return sum;
+}
---
+++ ExtraPractice/32.MiddleElementsMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] inputArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+double result = AverageOfMiddleElements(inputArray);
+
+Console.WriteLine($"{result:F2}");
+
+static double AverageOfMiddleElements(int[] Array)
+{
+    int middleDigit = Array.Length / 2;
+
+    double average = (Array[middleDigit] + Array[middleDigit - 1]) / 2.0;
+
+    return average;
+}
