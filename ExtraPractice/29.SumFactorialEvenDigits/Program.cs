0d82b7aec7f8650bef963ffa7d3ada80697cf670
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-10-30 11:47:13 +0200

    Updated
    Updated

---
+++ ExtraPractice/29.SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = int.Parse(Console.ReadLine()); //4532
+
+int sum = 0;
+while (number > 0)
+{
+    int lastDigit = number % 10;
+
+    if (lastDigit % 2 == 0 )
+    {
+        int currentSum = lastDigit;
+
+        for (int i = 1; i < lastDigit; i += 1)
+        {
+            currentSum *= i;
+        }
+        sum += currentSum;
+    }
+    number /= 10;
+}
+
+Console.WriteLine(sum);
---
+++ ExtraPractice/30.MiddleElements/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = Console.ReadLine() // 3 4 6 7 8 9
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int middleNum = numbers.Length / 2;
+
+double sum = numbers[middleNum - 1] + numbers[middleNum];
+
+double average = sum / 2;
+
+Console.WriteLine($"{average:F2}");
