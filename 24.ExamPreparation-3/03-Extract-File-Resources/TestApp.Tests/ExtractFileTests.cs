c2950733268822cb22e415a48f796b506315daaa
KDyulgerov <kdyulgerov.sport@gmail.com>
2023-11-04 22:02:40 +0200

    ThirdPreparation
    ThirdPreparation

---
+++ 24.ExamPreparation-3/03-Extract-File-Resources/TestApp.Tests/ExtractFileTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class ExtractFileTests
+{
+    [Test]
+    public void Test_GetFile_NullPath_ThrowsArgumentNullException()
+    {
+        string input = null;
+
+        Assert.That(() => ExtractFile.GetFile(input), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetFile_EmptyPath_ThrowsArgumentNullException()
+    {
+        string input = string.Empty;
+
+        Assert.That(() => ExtractFile.GetFile(input), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetFile_ValidPath_ReturnsFileNameAndExtension()
+    {
+        string input = "D:\\KD\\SoftUni\\03-Extract-File-Resources.txt";
+        string expected = "File name: 03-Extract-File-Resources\nFile extension: txt";
+
+        string result = ExtractFile.GetFile(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithNoExtension_ReturnsFileNameOnly()
+    {
+        string input = "D:\\KD\\SoftUni\\03-Extract-File-Resources";
+        string expected = "File name: 03-Extract-File-Resources";
+
+        string result = ExtractFile.GetFile(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithSpecialCharacters_ReturnsFileNameAndExtension()
+    {
+        string input = "D:\\K$D\\So_ft.Uni\\03-Extract-File-Resources.txt";
+        string expected = "File name: 03-Extract-File-Resources\nFile extension: txt";
+
+        string result = ExtractFile.GetFile(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ 24.ExamPreparation-3/04-Drum-Set-Resources/TestApp.Tests/DrumSetTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class DrumSetTests
+{
+    [Test]
+    public void Test_Drum_TerminateCommandNotGiven_ThrowsArgumentException()
+    {
+        decimal money = 100;
+        List<int> initialValues = new() { 1, 2, 10 };
+        List<string> commands = new() { "2", "2", "" };
+
+        Assert.That(() => DrumSet.Drum(money, initialValues, commands), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_Drum_StringGivenAsCommand_ThrowsArgumentException()
+    {
+        decimal money = 100;
+        List<int> initialValues = new() { 1, 1 };
+        List<string> commands = new() { "stringTest" };
+
+        Assert.That(() => DrumSet.Drum(money, initialValues, commands), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_Drum_ReturnsCorrectQualityAndAmount()
+    {
+        decimal money = 100;
+        List<int> initialValues = new() { 3, 3 };
+        List<string> commands = new() { "1", "1", "Hit it again, Gabsy!" };
+        string expected = "1 1\nGabsy has 100.00lv.";
+
+        string result = DrumSet.Drum(money, initialValues, commands);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Drum_BalanceZero_WithOneDrumLeftOver_ReturnsCorrectQualityAndAmount()
+    {
+        decimal money = 100;
+        List<int> initialValues = new() { 3, 3 };
+        List<string> commands = new() { "3", "3", "Hit it again, Gabsy!" };
+        string expected = "3 3\nGabsy has 64.00lv.";
+
+        string result = DrumSet.Drum(money, initialValues, commands);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+
+    [Test]
+    public void Test_Drum_NotEnoughBalance_RemovesAllDrums_ReturnsCorrectQualityAndAmount()
+    {
+        decimal money = 10;
+        List<int> initialValues = new() { 9, 9 };
+        List<string> commands = new() { "10", "10", "Hit it again, Gabsy!" };
+        string expected = "\nGabsy has 10.00lv.";
+
+        string result = DrumSet.Drum(money, initialValues, commands);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+}
